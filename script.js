ffcc01a3ea198a5657fbdf52f21db82f3b0fe6e0
Matthieu Basset <bassetmatt@gmail.com>
2025-06-27 18:29:54 +0200

    getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello, World!");
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+    let n = getRandomInt(3);
+    if (n === 0) {
+        return "rock";
+    } else if (n === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+for (let i = 0; i < 50; i++) {
+    let s = getComputerChoice()
+    console.log(s)
+}
